04ed252e146dd29aba69641833b4cd669268d657
Robbie Jaeger <jaeger.rob@gmail.com>
2016-06-02 23:23:22 -0600

    Add user and sessions with feature tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
+  root to: 'locations#index'
+
   resources :locations, only: [:index] do
     resources :hikes, only: [:index, :show]
   end
 
-  root to: 'locations#index'
+  resources :users, only: [:new, :create]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
