e84982e6f9f969cc1b6ed557f1036880ba6eb1a6
joshuajhun <joshuajhun@gmail.com>
2016-06-06 09:39:38 -0600

    i left some comments for you to look at

--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.feature "User can create account" do
   scenario "they create account and are logged in" do
+    # to make this test a little more verbose maybe look into haveing
+    # the user start at the home page checking if the page has a sign up button
+
     visit new_user_path
 
     expect(page).to have_content "Login"
